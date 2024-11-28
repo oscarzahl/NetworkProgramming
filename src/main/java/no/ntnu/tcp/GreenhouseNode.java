e9ed2029dffb41c8a6ae50a17ec30cc1d977f8c6
Jerezard <jeremys-1999@hotmail.com>
2024-11-28 15:50:54 +0100

    Send ActuatorData to the server

--- src/main/java/no/ntnu/tcp/GreenhouseNode.java
+++ src/main/java/no/ntnu/tcp/GreenhouseNode.java
@@ -3,8 +3,11 @@ package no.ntnu.tcp;
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import no.ntnu.greenhouse.Actuator;
+import no.ntnu.greenhouse.ActuatorCollection;
 import no.ntnu.greenhouse.Sensor;
 
 public class GreenhouseNode {
@@ -12,12 +15,14 @@ public class GreenhouseNode {
     private final String serverAddress;
     private final int port;
     private final List<Sensor> sensors;
+    private final ActuatorCollection actuators;
 
-    public GreenhouseNode(int nodeId, String serverAddress, int port, List<Sensor> sensors) {
+    public GreenhouseNode(int nodeId, String serverAddress, int port, List<Sensor> sensors, ActuatorCollection actuators) {
         this.nodeId = nodeId;
         this.serverAddress = serverAddress;
         this.port = port;
         this.sensors = sensors;
+        this.actuators = actuators;
     }
 
 
@@ -34,6 +39,12 @@ public class GreenhouseNode {
                         String sensorData = generateSensorData();
                         out.println("SENSOR:" + nodeId + ":" + sensorData);
                         System.out.println("Sent: " + sensorData);
+
+                        String actuatorData = generateActuatorData();
+                        out.println("ACTUATOR:" + nodeId + ":" + actuatorData);
+                        System.out.println("Sent: " + actuatorData);
+                        
+
                         Thread.sleep(5000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
@@ -48,6 +59,21 @@ public class GreenhouseNode {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String generateActuatorData() {
+        StringBuilder builder = new StringBuilder();
+        Iterator<Actuator> iterator = actuators.iterator();
+
+        while(iterator.hasNext()){
+            Actuator actuator = iterator.next();
+            builder.append(actuator.getType())
+                   .append("=")
+                   .append(actuator.isOn())
+                   .append(",");
+        }
+
+        return builder.substring(0,builder.length() -1);
     }
 
     private String generateSensorData() {
